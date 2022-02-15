a1d20f4bb9c6b3692d2c3a3b1925c8724ee1a21f
Adam Le Roux <adamleroux26@gmail.com>
2022-02-15 21:29:42 +0100

    fin defi

--- siteliste/forms.py
+++ siteliste/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class NameForm(forms.Form):
     titre = forms.CharField(label='Titre', max_length=100)
     desc = forms.CharField(label='Desc', max_length=100)
-    donner = forms.CharField(label='donner', max_length=100)
+    donneur = forms.CharField(label='donneur', max_length=100)
 
