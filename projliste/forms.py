e664cc3706c00a28e43e7cd1597f3c703e202a6f
Dorian <gaspardori@cy-tech.fr>
2022-02-24 00:51:42 +0100

    formulaire défi stylisé

--- projliste/forms.py
+++ projliste/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from captcha.fields import ReCaptchaField
 class NameForm(forms.Form):
     titre = forms.CharField(label='Titre', max_length=100)
-    desc = forms.CharField(label='Desc', max_length=100)
+    desc = forms.CharField(label='Desc', max_length=200)
     donneur = forms.CharField(label='donneur', max_length=100)
     captcha=ReCaptchaField()
